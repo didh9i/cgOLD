96d6a1bf773bc548e2cc9655d5eded13c42c6b3f
dpetrov <dpetrov@smartdatatech.io>
2021-08-27 16:46:21 +0300

    Graph: Scroll - added more steps; Graph - connections added; Branch recursion added;

--- graph/graph.js
+++ graph/graph.js
@@ -1,4 +1,23 @@
-const container = document.querySelector(".mPage");
+const mPage = document.querySelector(".mPage");
+const container = document.createElement("div");
+container.style.width = "100%";
+container.style.height = "100%";
+mPage.appendChild(container);
+const newCanvas = document.createElement("canvas");
+// Создаем Фон
+
+newCanvas.width = 2048;
+newCanvas.height = 2048;
+newCanvas.style.position = "absolute";
+newCanvas.style.zIndex = -5;
+newCanvas.style.left = "50%";
+newCanvas.style.top = "50%";
+newCanvas.style.transform = "translate(-50%, -50%)";
+newCanvas.style.pointerEvents = "none";
+newCanvas.style.border = "darkred solid 16px";
+
+const ctx = newCanvas.getContext('2d');
+const ctxCenter = 1024;
 
 function readTextFile(file, callback) {
     let rawFile = new XMLHttpRequest();
@@ -21,26 +40,30 @@ readTextFile("./graph/graph.json", function (text) {
 
 function generateGraph(graph) {
     // Создаем корневой элемент:
-    if (!graph.img)                     // Исключаем ошибки не существующих данных
-        graph.img = null;
-    if (!graph.branches)
-        graph.branches = [];
-    if (!graph.link)
-        graph.link = "./index.html";
                                         // Создаем Контейнер и Картинку внутри него
     const newNode = document.createElement("button");
+    const newNodeBack = document.createElement("div");
     const newNodeImg = document.createElement("img");
 
+    newNodeBack.style.position = "absolute";
+
+    newNodeBack.style.width = "100%";
+    newNodeBack.style.height = "100%";
+    newNodeBack.style.background = "#2B2B2B";
+    newNodeBack.style.zIndex = "-1";
     newNode.transitionX = 0;
     newNode.transitionY = 0;
     newNode.addEventListener("mousedown", () => {window.location.href = graph.link});
     newNode.constTransitionX = newNode.transitionX;
     newNode.constTransitionY = newNode.transitionY;
+    newNode.style.border = "#434445 solid 2px";
     newNodeImg.src = graph.img;         // Передаем расположение иконки
     newNode.appendChild(newNodeImg);    // Вписываем картиркинку в контейнер
-    newNode.id = "mainNode";                // Id
+    newNode.id = "mainNode";            // Id
 
     container.appendChild(newNode);     // Вписываем Контейнер в страничку
+    newNode.appendChild(newCanvas);
+    newNode.appendChild(newNodeBack);
 
     // Запускаем рекурсию создания веток
     generateBranches(graph, newNode);
@@ -55,67 +78,83 @@ function generateBranches(branch, father, angle = Math.PI * 2, startAngle = 0) {
     //console.log("a: " + a);
     //console.log("l: " + l);
     while (i < branch.branches.length) {
-        let x = Math.sin(startAngle + a * i) * l;
-        let y = Math.cos(startAngle + a * i) * l;
+        let x = father.transitionX + Math.sin(startAngle + a * i) * l;
+        let y = father.transitionY + Math.cos(startAngle + a * i) * l;
 
         //console.log("x: " + x);
         //console.log("y: " + y);
         // Создаем Контейнер и Картинку внутри него
         const newNode = document.createElement("div");
         const newNodeImg = document.createElement("img");
+        // <svg><line x1="50" y1="50" x2="350" y2="350" stroke="white" stroke-width="3"></line></svg>
 
+        ctx.moveTo(ctxCenter + father.transitionX, ctxCenter + father.transitionY);
+        ctx.lineTo(ctxCenter + x, ctxCenter + y);
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = '#ffffff';
+        ctx.stroke();
         newNode.transitionX = x;
         newNode.transitionY = y;
         newNode.constTransitionX = newNode.transitionX;
         newNode.constTransitionY = newNode.transitionY;
 
         newNode.addEventListener("mousedown", () => {window.location.href = branch.link});
-        newNodeImg.src = "assets/icoCode.svg";//Передаем расположение иконки
-        newNode.appendChild(newNodeImg);    // Вписываем картиркинку в контейнер
-        newNode.id = "node";                // Id
+        newNodeImg.src = "assets/icoCode.svg";  //Передаем расположение иконки
+        newNode.appendChild(newNodeImg);        // Вписываем картиркинку в контейнер
+        newNode.id = "node";                    // Id
         newNode.style.transform = "translate(" + x + "px," + y + "px)";
 
         //console.log(newNode.style.transform);
-        container.appendChild(newNode);     // Вписываем Контейнер в страничку
+        container.appendChild(newNode);         // Вписываем Контейнер в страничку
+        console.log(newNode);
+        if (branch.branches[i].branches.length >= 1)
+            generateBranches(branch.branches[i], newNode, a * 2, a * i - a / 2);
         i++;
     }
 }
 
 let mouseStartX = 0;
 let mouseStartY = 0;
+let graphScale = 1;
 
-container.addEventListener("touchstart", function(event) {
+mPage.addEventListener("touchstart", function(event) {
     mouseStartX = (event.pageX === undefined) ? event.targetTouches[0].pageX : event.pageX;
     mouseStartY = (event.pageY === undefined) ? event.targetTouches[0].pageY : event.pageY;
 
     //console.log(event);
-    container.addEventListener("touchmove", MouseMoveHandler);
-    container.addEventListener("touchend", disableMoveHandler);
-    container.addEventListener("mouseleave", disableMoveHandler);
+    mPage.addEventListener("touchmove", MouseMoveHandler);
+    mPage.addEventListener("touchend", disableMoveHandler);
+    mPage.addEventListener("mouseleave", disableMoveHandler);
     //console.log("Touch Start");
 });
 
-container.addEventListener("mousedown", function(event) {
+mPage.addEventListener("mousedown", function(event) {
     mouseStartX = (event.pageX === undefined) ? event.targetTouches[0].pageX : event.pageX;
     mouseStartY = (event.pageY === undefined) ? event.targetTouches[0].pageY : event.pageY;
-    container.addEventListener("mousemove", MouseMoveHandler);
-    container.addEventListener("mouseup", disableMoveHandler);
-    container.addEventListener("mouseleave", disableMoveHandler);
+
+    mPage.addEventListener("mousemove", MouseMoveHandler);
+    mPage.addEventListener("mouseup", disableMoveHandler);
+    mPage.addEventListener("mouseleave", disableMoveHandler);
     //console.log("MouseDown");
 });
 
-container.addEventListener("wheel", function (event) {
+
+mPage.addEventListener("wheel", function (event) {
     console.log("Scroll...");
     console.log(event.deltaY);
     if (event.deltaY === -100)
-        container.style.transform = "scale(2)";
+        graphScale *= 1.1;
     else
-        container.style.transform = "scale(1)";
+        graphScale /= 1.1;
+
+    container.style.transform = "scale(" + graphScale + ")";
 })
 
 function disableMoveHandler(event) {
     let mouseOffsetX = ((event.pageX === undefined) ? event.changedTouches[0].pageX : event.pageX) - mouseStartX;
     let mouseOffsetY = ((event.pageY === undefined) ? event.changedTouches[0].pageY : event.pageY) - mouseStartY;
+    mouseOffsetX /= graphScale;
+    mouseOffsetY /= graphScale;
 
     for (const element of container.children) {
         element.transitionX = mouseOffsetX + element.transitionX;
@@ -125,17 +164,19 @@ function disableMoveHandler(event) {
         //console.log(transformX);
         //console.log(transformY);
     }
-    container.removeEventListener("touchmove", MouseMoveHandler);
-    container.removeEventListener("touchend", disableMoveHandler);
-    container.removeEventListener("mousemove", MouseMoveHandler);
-    container.removeEventListener("mouseup", disableMoveHandler);
-    container.removeEventListener("mouseleave", disableMoveHandler);
+    mPage.removeEventListener("touchmove", MouseMoveHandler);
+    mPage.removeEventListener("touchend", disableMoveHandler);
+    mPage.removeEventListener("mousemove", MouseMoveHandler);
+    mPage.removeEventListener("mouseup", disableMoveHandler);
+    mPage.removeEventListener("mouseleave", disableMoveHandler);
     //console.log("MouseLeave");
 }
 
 function MouseMoveHandler (event) {
     let mouseOffsetX = ((event.pageX === undefined) ? event.targetTouches[0].pageX : event.pageX) - mouseStartX;
     let mouseOffsetY = ((event.pageY === undefined) ? event.targetTouches[0].pageY : event.pageY) - mouseStartY;
+    mouseOffsetX /= graphScale;
+    mouseOffsetY /= graphScale;
 
     //console.log("Move");
     //console.log(container.children);
@@ -149,11 +190,11 @@ function MouseMoveHandler (event) {
 }
 
 window.addEventListener('resize', function () {
-    container.removeEventListener("touchmove", MouseMoveHandler);
-    container.removeEventListener("touchend", disableMoveHandler);
-    container.removeEventListener("mousemove", MouseMoveHandler);
-    container.removeEventListener("mouseup", disableMoveHandler);
-    container.removeEventListener("mouseleave", disableMoveHandler);
+    mPage.removeEventListener("touchmove", MouseMoveHandler);
+    mPage.removeEventListener("touchend", disableMoveHandler);
+    mPage.removeEventListener("mousemove", MouseMoveHandler);
+    mPage.removeEventListener("mouseup", disableMoveHandler);
+    mPage.removeEventListener("mouseleave", disableMoveHandler);
     for (const element of container.children) {
         element.transitionX = element.constTransitionX;
         element.transitionY = element.constTransitionY;
